3f898fd911af8847ab3bd2d4ab9f04e14f4e1789
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-08-15 21:15:24 +0100

    Joining api errors with comma

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -35,16 +35,15 @@ func (p *plugin) Find(ctx context.Context, req *secret.Request) (*drone.Secret, 
 		name = "value"
 	}
 
-	// makes an api call to the aws secrets manager and attempts
+	// makes an api call to vault and attempts
 	// to retrieve the secret at the requested path.
 	params, err := p.find(path)
 	if err != nil {
 		if err, ok := err.(*api.ResponseError); ok {
 			var builder strings.Builder
 			for _, e := range err.Errors {
-				builder.WriteString("* " + e)
+				builder.WriteString(", " + e)
 			}
-			builder.WriteString(" *")
 			return nil, errors.New(fmt.Sprintf("unexpected response %d from vault: %s", err.StatusCode, builder.String()))
 		}
 		return nil, errors.New(fmt.Sprintf("error finding secret: %s", err.Error()))
